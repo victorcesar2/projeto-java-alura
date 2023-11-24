3a9cf74874ca32f9f8aca40bd07c45d0b62eff0b
victorcesar2 <vitor.c.j@hotmail.com>
2023-11-23 22:07:22 -0300

    incio-projeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+        /*System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluindoNoPlano = true;
+        double notaDoFilme = 8.1;
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = "Filme de aventeura com o galã dos anos 80";
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media /2);
+        System.out.println(classificacao);*/
+
+        int temperatura = 45;
+        double tempFahrenheit;
+
+        tempFahrenheit = (temperatura * 1.8) + 32;
+        System.out.println(tempFahrenheit);
     }
 }
